43b72d20f506cf28781a682246ed2e7e18374b90
tcslyrica <88646845+tcslyrica@users.noreply.github.com>
2021-08-23 18:25:25 +0000

    Update EmployeeEntity.java

--- src/main/java/com/hibernate1/EmployeeEntity.java
+++ src/main/java/com/hibernate1/EmployeeEntity.java
@@ -68,61 +68,9 @@ public class EmployeeEntity implements Serializable {
 		this.lastName = lastName;
 	}
  
-    // Accessors and mutators for all four fields
+
     
     
     
 }
-//@Entity
-//public class EmployeeEntity {
-//	
-//		@Id
-//		@GeneratedValue(strategy = GenerationType.IDENTITY)
-//		private Integer id;
-//		private String name;
-//		private int age;
-//		private Date dob;
-//		private String streetName;
-//		
-//
-//		public String getStreetName() {
-//			return streetName;
-//		}
-//
-//		public void setStreetName(String streetName) {
-//			this.streetName = streetName;
-//		}
-//
-//		public Date getDob() {
-//			return dob;
-//		}
-//
-//		public void setDob(Date dob) {
-//			this.dob = dob;
-//		}
-//
-//		public int getAge() {
-//			return age;
-//		}
-//
-//		public void setAge(int age) {
-//			this.age = age;
-//		}
-//
-//		public String getName() {
-//			return name;
-//		}
-//
-//		public void setName(String name) {
-//			this.name = name;
-//		}
-//
-//		public Integer getId() {
-//			return id;
-//		}
-//
-//		public void setId(Integer id) {
-//			this.id = id;
-//}
-//}
-//
+
